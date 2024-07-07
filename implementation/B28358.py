fae507c740ac98a0533163ba60637d0577bc0674
hwangbo98 <48913437+hwangbo98@users.noreply.github.com>
2024-07-07 18:04:48 +0900

    Add problem: 백준 28358번

--- implementation/B28358.py
+++ implementation/B28358.py
@@ -5,7 +5,10 @@ input = sys.stdin.readline
 def is_valid_date(month, day, forbidden_nums) :
     month_str = f"{month}"
     day_str = f"{day}"
-
+    # 여기서 고민해야 하는 부분은 한 가지임. 
+    # 06월 06일은 0을 포함하지만 여기서 원하는 부분은 이런 건 고민하지 않는 거임.
+    # 10월이나 10일, 20, 30일 같이 뒤에 0이 붙는 거는 OK 
+    # 위의 경우는 XXXX
     for k in month_str + day_str :
         if int(k) in forbidden_nums :
             return False
@@ -36,3 +39,4 @@ for _ in range(T) :
 for k in outputs :
     print(k)
 
+#test
