2c9217a95a33f2d43d333f91b3215be66f95c867
hwangbo98 <48913437+hwangbo98@users.noreply.github.com>
2024-07-08 12:59:46 +0900

    Add problem: 백준 5635번

---
+++ implementation/B5635.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+birthday_dict = {}
+for _ in range(N) :
+    name, day, month, year = map(str, input().split())
+    month = int(month)
+    day = int(month)
+    new_month = f"{month:02d}"
+    new_day = f"{day:02d}"
+    birthday_dict[name] = year + new_month + new_day
+
+
+sorted_birthday = sorted(birthday_dict.items(), key = lambda x : x[1])
+
+print(sorted_birthday[0][0])
+print(sorted_birthday[-1][0])
