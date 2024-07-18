bb67e54ce0ca08d9ef7c747f7b3272cbac820b3f
hwangbo98 <48913437+hwangbo98@users.noreply.github.com>
2024-07-18 23:20:30 +0900

    Add problem: 백준 9655번

--- DP/B9655.py
+++ DP/B9655.py
@@ -6,9 +6,9 @@ N = int(input())
 
 dp = [0] * (N+1)
 
-dp[0] = 0
-dp[1] = 1
-dp[2] = 0
+dp[1] = 0
+dp[2] = 1
+dp[3] = 0
 
 for i in range(4, N+1) :
     if dp[i-1] == 1 or dp[i-3] == 1 :
@@ -16,4 +16,4 @@ for i in range(4, N+1) :
     else :
         dp[i] = 1
 
-print("SK" if dp[N] == 1 else "CY")
+print("SK" if dp[N] == 0 else "CY")
